5377c9a76c1e84d8afe15c6c6c1ad9186b1f5bb0
ifeanyi chimbo <ifykenchi@gmail.com>
2024-08-13 18:13:42 +0100

    Ecommerce Store

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Flasher\Prime\FlasherInterface;
+
+class AdminController extends Controller
+{
+    public function view_category()
+    {
+        $data = Category::all();
+
+        return view('admin.category', compact('data'));
+    }
+
+    public function add_category(Request $request)
+    {
+        $category = new Category();
+
+        $category->category_name = $request->category;
+
+        $category->save();
+
+        flash()->option('timeout', 3000)->success('Category Added Successfully');
+
+        return redirect()->back();
+    }
+
+    public function delete_category($id)
+    {
+        $data = Category::find($id);
+
+        $data->delete();
+
+        flash()->option('timeout', 3000)->success('Category Deleted Successfully');
+
+        return redirect()->back();
+    }
+
+    public function edit_category($id)
+    {
+        $data = Category::find($id);
+
+        return view('admin.edit_category', compact('data'));
+    }
+
+    public function update_category(Request $request, $id)
+    {
+        $data = Category::find($id);
+        $data->category_name = $request->category;
+        $data->save();
+        flash()->option('timeout', 3000)->success('Category Updated Successfully');
+        return redirect('/view_category');
+    }
+
+    public function add_product()
+    {
+        $category = Category::all();
+
+        return view('admin.add_product', compact('category'));
+    }
+
+    public function upload_product(Request $request)
+    {
+        $data = new Product();
+
+        $data->title = $request->title;
+
+        $data->description = $request->description;
+
+        $data->price = $request->price;
+
+        $data->quantity = $request->qty;
+
+        $data->category = $request->category;
+
+        $image = $request->image;
+
+        if ($image) {
+            $imagename = time() . '.' . $image->getClientOriginalExtension();
+
+            $request->image->move('products', $imagename);
+
+            $data->image = $imagename;
+        }
+
+        $data->save();
+
+        flash()->option('timeout', 3000)->success('Product Added Successfully');
+
+        return redirect()->back();
+    }
+}
+
